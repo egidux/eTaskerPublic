c018a941248e7ef1084da9f0e95c3ffe5df5914c
egidux <egidux@mail.ru>
2016-10-07 19:40:53 +0100

    added log$j

--- src/main/java/eTasker/main/Main.java
+++ src/main/java/eTasker/main/Main.java
@@ -1,9 +1,13 @@
 package eTasker.main;
 
+import org.apache.log4j.Logger;
+
 public class Main {
-
+	final static Logger lOGGER = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-
+		lOGGER.debug("Starting main class");
+		System.out.println("Hello");
+		lOGGER.info("Finishing main class");
 	}
-
 }
