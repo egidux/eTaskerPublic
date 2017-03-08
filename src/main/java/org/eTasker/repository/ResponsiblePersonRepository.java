809ed0016f3c89164202d6f602c22538a898cb00
egidux <egidux@mail.ru>
2017-03-08 15:25:34 +0000

    deleted project

--- src/main/java/org/eTasker/repository/ResponsiblePersonRepository.java
+++
@@ -1,9 +0,0 @@
-package org.eTasker.repository;
-
-import org.eTasker.model.Responsible_person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResponsiblePersonRepository extends JpaRepository<Responsible_person, Long> {
-}
--- src/main/java/org/eTasker/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package org.eTasker.repository;
-
-import org.eTasker.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
