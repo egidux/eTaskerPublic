d9215180cc6ae5a0ecffab2be4202e61d85f7431
egidux <egidux@mail.ru>
2017-03-08 15:45:54 +0000

    deleted project

--- app/app/src/main/java/org/etasker/tool/TaskType.java
+++
@@ -1,6 +0,0 @@
-package org.etasker.tool;
-
-
-public enum TaskType {
-    Unassigned, Assigned, InProgress, Done, Aborted, Stopped;
-}
--- app/app/src/main/java/org/etasker/tool/Time.java
+++
@@ -1,7 +0,0 @@
-package org.etasker.tool;
-
-import java.math.BigInteger;
-
-public class Time {
-    public static long time;
-}
